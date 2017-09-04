a4f4a643a6494411f95dec30ca5cb1fdf5e8fd8b
test <chris@intelatek.com>
2017-09-04 17:38:48 +0000

    powerball

--- powerball.rb
+++ powerball.rb
@@ -1,5 +1,4 @@
-puts "In powerball, 5 white balls are selected from the 69 white balls which are numbered from 1 to 69.  One red 'Powerball' is also sected from 26 red balls which are numbered 1 to 26.  This powerball number generator can randomly produce those numbers for you. Good Luck!"
-puts ""
+puts "In powerball, 5 white balls are selected from the 69 white balls which are numbered from 1 to 69.  One red 'Powerball' is also sected from 26 red balls which are numbered 1 to 26.  This powerball number generator can randomly produce those numbers for you. Good Luck!\n"
 
 white = (1..69).to_a
 powerball = (1..26).to_a
@@ -7,29 +6,10 @@ powerball = (1..26).to_a
 loop do
   print white.shuffle.first(5).sort.join(' | ')
   powerball = powerball.shuffle
-  print " | Powerball: #{powerball[0]}"
-  puts ""
-  puts ""
-  puts "Would you like new numbers? (Y/N)"
+  print " | Powerball: #{powerball[0]}\n\n"
+  puts "Would you like new numbers? (Y/N)\n"
   answer = gets.chomp.upcase
   if answer != "Y"
-  break
+    break
   end
 end
-
-
-# I have written three slightly different ways to accomplish the same results.  I start out by creating an array with 69 numbers and another with 26 numbers and then in each of the three examples I'm shuffling and then grabbing the first 5 elements in the array. In the first example, I'm joining the first 5 elements but in the other two examples I'm itterating over
-
-# Another way to do the same thing
-# white.shuffle.first(5).sort.each {|n| print "#{n} | "}
-# powerball = powerball.shuffle
-# print "Powerball: #{powerball[0]}"
-# puts ""
-
-# And another way to do the same thing
-# white.shuffle.first(5).sort.each do |number|
-#   print "#{number} | "
-# end
-# powerball = powerball.shuffle
-# print "Powerball: #{powerball[0]}"
-# puts ""
